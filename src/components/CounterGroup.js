aa595be7d68f446dcbfceb9c0386c6435385d191
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-13 16:13:28 +0800

    [counter][koby] change CounterGroup method

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -1,14 +1,11 @@
 import Counter from "./Counter";
 function CounterGroup(props){
     return(
-        // <>  
-        //     {/* <span>{props.size}</span> */}
-        //     {[...Array(props.size)].map(() => <Counter></Counter>)}
-        // </>
-        Array.from(
-            {length: props.size},
-            () => <Counter></Counter>
-        )
+        <>
+        {Array.from(
+            {length: props.size}
+        ).map(()=><Counter></Counter>)}
+        </>
     )
 }
 
